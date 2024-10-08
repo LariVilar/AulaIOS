ce9ac36931b0a12d5101c737cb8645bf89805843
Gabriel Alvarenga <gabriel.alvarenga861@gmail.com>
2024-10-08 08:08:15 -0300

    salvar

---
+++ JavaScript/Aula 01/Exercicio.js
@@ -0,0 +1,14 @@
+let nome, nota01, nota02, nota03;
+
+nome = Larissa
+nota01 = 0.8;
+nota02 = 0.7;
+media = ((nota01 + nota02)) / 2
+
+console.log("")
+
+let valor1 = `A`
+let valor2 = `B`
+let troca;
+
+console.log(valor1, valor2)
---
+++ JavaScript/Aula 01/Main.js
@@ -0,0 +1,8 @@
+let nota01, nota02, nota03;
+
+nota01 = 10;
+nota02 = 6;
+media = nota01 + nota02 / 2
+
+
+console.log("A média do aluno é: ${media}")
