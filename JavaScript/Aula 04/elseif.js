910f363bdd94c799563510bf6650e2ef3ba5655b
Larissa <Larissa.vilar06@outlook.com>
2024-10-14 11:04:38 -0300

    salvar

---
+++ JavaScript/Aula 04/elseif.js
@@ -0,0 +1,19 @@
+let nota1, nota2, media, faltas
+nota1 = 10
+nota2 = 5
+faltas =  3
+media = (nota1 + nota2)/2
+
+if(media >= 7 && faltas <= 5)
+//true
+
+{
+    console.log(`O jovem está aprovado com a média ${media} e quantidade de ${faltas}faltas`);
+
+    console.log(`O jovem está reprovado com a média ${media} e quantidade de ${faltas}faltas`);
+    //true
+}else{
+
+}
+
+console.log(`fim`);
